adb51b6af7146987ffd4cf938419640e73d15b2b
Boopathi <boopathig158@gmail.com>
2024-06-25 19:19:00 +0530

    updated setup.py and created my_cool_test_method

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+     name='math_operations',
+    version='0.1',
+    # packages=find_packages(),
+    description='A simple math operations library',
+    author='Boopathi',
+    author_email='boopathig158@gmail.com',
+    url='https://github.com/Bobby-13/custom_packages',
+    license='unlicense',
+    packages=['ptolemaios'],
+    zip_safe=False
+)
