9b6b4feafca7336307f83eb903cf897d7eca0ccd
Boopathi <boopathig158@gmail.com>
2024-06-25 19:22:49 +0530

    code level changes in setup.py file

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 
+from setuptools import setup, find_packages
+
 setup(
-     name='math_operations',
+    name='math_functions',
     version='0.1',
-    # packages=find_packages(),
+    packages=find_packages(),
     description='A simple math operations library',
     author='Boopathi',
     author_email='boopathig158@gmail.com',
     url='https://github.com/Bobby-13/custom_packages',
-    license='unlicense',
-    packages=['ptolemaios'],
-    zip_safe=False
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
